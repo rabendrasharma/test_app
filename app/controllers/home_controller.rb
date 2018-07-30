c359aba2bb669ab7ffcd487cf54e175c25611483
rabendra sharma <rabendrasharma007@gmail.com>
2018-07-30 12:58:42 +0530

    #Feature code settings done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,17 +2,4 @@ class HomeController < ApplicationController
   def index
   	@images = current_user.images.all
   end
-
-  # def create
-  # 	params[:image][:avatar].each do |image|
-  # 	 	current_user.images.create(avatar: image)
-  # 	end
-  # 	redirect_to root_path
-  # end
-
-  private
-
-  def image_params
-  	params.require(:image).permit(:avatar)
-  end
 end
